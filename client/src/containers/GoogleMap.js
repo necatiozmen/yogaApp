f5cadb3da7eaa60d131696f5bb37cd968c0b2edb
necatiozmen$ git config --global user.email necati.ozmen.9@gmail.comgit configgit config --global user.name necatiozmen <necati.ozmen.9@gmail.com>
2018-05-30 22:01:55 +0200

    feat: markers drop implemented

--- client/src/containers/GoogleMap.js
+++ client/src/containers/GoogleMap.js
@@ -1,22 +1,58 @@
+/*global google*/
 import React from 'react';
 import { compose, withProps } from 'recompose';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 import { connect } from 'react-redux';
 
-const MyMapComponent = (props) =>
+class MyMapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: 42,
+      lng: 23,
+    };
+  }
 
-  <GoogleMap
-    defaultZoom={5}
-    defaultCenter={{ lat: 41, lng: 2 }}
-  >
+//    zoomMarkers = () => {
+//      map => {
+//     const bounds = new window.google.maps.LatLngBounds();
+//     if (this.props.studios.length > 0) {
+//       bounds.extend(new window.google.maps.LatLng(
+//       this.props.studios[0].coordinates.latitude, this.props.studios[0].coordinates.longitude));
+//       console.log(bounds);
+//     }
+//     map.fitBounds(bounds);
+//   }
+// }
+  render() {
+    const bounds = new window.google.maps.LatLngBounds();
+    if (this.props.studios.length > 0) {
+      bounds.extend(new window.google.maps.LatLng(
+      this.props.studios[0].coordinates.latitude, this.props.studios[0].coordinates.longitude));
+      console.log(bounds);
+    }
+    else {
+      bounds.extend(new window.google.maps.LatLng(
+      41.3, 2));
+    }
 
- {props.studios.map(studio =>
-  <Marker opacity	= {studio.id === props.id ? 1 : 0.4}key={studio.id}
-    position={{ lat: studio.coordinates.latitude, lng: studio.coordinates.longitude }} />
-
-)}
-  </GoogleMap>;
-
+    return (
+    <GoogleMap
+      defaultZoom={5}
+      defaultCenter={{ lat: 41, lng: 2 }}
+      ref={map => map && map.panToBounds(bounds, { right: 150 })}>
+      {/* // ref={this.zoomMarkers}> */}
+      {this.props.studios.map(studio =>
+        <Marker
+          opacity	= {studio.id === this.props.id ? 1 : 0.4}
+          key={studio.id}
+          position={{ lat: studio.coordinates.latitude, lng: studio.coordinates.longitude }}
+          animation={google.maps.Animation.DROP}/>
+        )}
+    </GoogleMap>
+);
+  }
+}
 const mapStateToProps = (state) => ({
   id: state.coords,
   studios: state.studios,
